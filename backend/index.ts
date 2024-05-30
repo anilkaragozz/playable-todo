fb7b7a59866965db60d959cc26036da5aec11cdd
anilkaragoz <anil.karagoz@d-teknoloji.com.tr>
2024-05-30 18:23:12 +0300

    feat(todo-app): add package's and renew app

--- backend/index.ts
+++ backend/index.ts
@@ -2,19 +2,23 @@ const dotenv = require("dotenv");
 const express = require("express");
 const databaseConnection = require("./config/db");
 const authRouter = require("./routes/auth");
-const authMiddleware = require("./middleware/auth");
 const todoRouter = require("./routes/todo");
+const authVerification = require("./middleware/auth");
 
 dotenv.config();
 const port = process.env.PORT || 8000;
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 databaseConnection();
 
 app.use("/auth", authRouter);
-app.use("/todo", todoRouter);
+
+app.use("/todo", authVerification, todoRouter);
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
